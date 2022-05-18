6e8929b0f3632aa0125c06d96e08b48113d3285c
AlexandraPast <90218758+AlexandraPast@users.noreply.github.com>
2022-05-19 01:34:24 +0300

    Add files via upload

---
+++ Assignment3_Transfer_learning/setup.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+pip install --upgrade pip
+pip install argparse
+pip install opencv-python scikit-learn tensorflow tensorboard pydot
+pip install scikeras pandas
+pip install Pillow
+pip install seaborn
+sudo apt-get update
+sudo apt-get -y install graphviz
