8024ed82e1c925569a8eef7ee1b580dd56fcbae7
AlexandraPast <90218758+AlexandraPast@users.noreply.github.com>
2022-05-19 07:29:21 +0300

    Add files via upload

---
+++ Assignment1_Image_Search/setup.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+pip install --upgrade pip
+pip install argparse
+pip install opencv-python scikit-learn tensorflow tensorboard pydot
+pip install scikeras pandas
+pip install Pillow
+pip install seaborn
+pip install tensorflow-hub
+sudo apt-get update
+sudo apt-get -y install graphviz
